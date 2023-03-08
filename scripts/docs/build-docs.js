0f5a6dff14bc477749fd3171cba44dfa709f007e
theajack <1506282385@qq.com>
2023-03-08 08:06:24 +0800

    fix: ci build

--- scripts/docs/build-docs.js
+++ scripts/docs/build-docs.js
@@ -3,10 +3,11 @@
  * @Date: 2022-11-20 18:49:51
  * @Description: Coding something
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-02-21 23:16:31
+ * @LastEditTime: 2023-03-08 08:05:42
  */
 const fs = require('fs');
-const { resolveRootPath } = require('../helper/utils');
+const { resolveRootPath, writeJsonIntoFile } = require('../helper/utils');
+const pkg = require('../../packages/sener/package.json');
 
 if (!fs.existsSync(resolveRootPath('docs')))
     fs.mkdirSync(resolveRootPath('docs'));
@@ -19,3 +20,9 @@ fs.copyFileSync(
     resolveRootPath('.gitignore'),
     resolveRootPath('docs/.gitignore')
 );
+
+// ! github ci 构建中 会有一个莫名的 undefined 依赖不知道哪里来的
+if (pkg.dependencies['undefined']) {
+    delete pkg.dependencies['undefined'];
+    writeJsonIntoFile(pkg, '#sener/package.json');
+}
