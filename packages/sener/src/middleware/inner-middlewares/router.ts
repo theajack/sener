5cf5e43c95b42aa5e1d763ce72493f4e8c131375
theajack <1506282385@qq.com>
2023-04-11 09:58:28 +0800

    feat: 1

--- packages/sener/src/middleware/inner-middlewares/router.ts
+++ packages/sener/src/middleware/inner-middlewares/router.ts
@@ -11,13 +11,19 @@ import {
     IMiddleWareResponseData,
 } from 'sener-types';
 
-export type IRouter = IJson<IRouterHandler>;
+export type IRouter = IJson<IRouterHandler|IRouterHandlerData>;
 
 export type IRouterHandler = (
     data: IMiddleWareResponseData,
 ) => IPromiseMayBe<IMiddleWareResponseReturn|ICommonReturn>;
 
+export type IRouterHandlerData = {
+    exe: IRouterHandler;
+    meta: IJson;
+}
+
 interface IRouterHelper {
+    meta: IJson;
     index: ()=>number;
     route(
         url: string, data?: Partial<IMiddleWareRequestData>,
@@ -31,7 +37,7 @@ declare module 'sener-types-extend' {
 const REG = /^(post|get|put|delete):/;
 
 export class Router extends MiddleWare {
-    routers: IRouter = {};
+    routers: IJson<IRouterHandlerData> = {};
 
     static _routers: IRouter = {};
     static Add (routers: IRouter) {
@@ -47,10 +53,40 @@ export class Router extends MiddleWare {
     constructor (routers: IRouter) {
         super();
         for (const k in routers) {
-            const key = this.fillUrl(k);
-            this.routers[key] = routers[k];
+            const value = routers[k];
+            if(typeof value === 'function'){
+                const {meta, url} = this.extractMeta(k);
+                const key = this.fillUrl(url);
+                this.routers[key] = {
+                    exe: value,
+                    meta: meta
+                }
+            }else{
+                const key = this.fillUrl(k);
+                this.routers[key] = value;
+            }
         }
         // console.log(this.routers);
+    }
+
+    private extractMeta(url: string){
+        if(!url.startsWith('[')) return {meta:{}, url}
+        const arr = url.matchAll(/(?<=[\[\&])(.*?)(=(.*?))?(?=[\&\]])/g);
+
+        const meta: IJson = {};
+
+        for(let item of arr){
+            const key = item[1];
+            let value: any = item[2];
+            if(typeof value === 'undefined'){
+                value = true;
+            }
+            meta[key] = value
+        }
+        return {
+            meta,
+            url: url.replace(/^\[.*?\]/, '')
+        }
     }
 
     private fillUrl (k: string) {
@@ -65,10 +101,12 @@ export class Router extends MiddleWare {
         return `${(method || 'get').toLocaleLowerCase()}:${url}`;
     }
 
-    request ({ url, method, send404 }: IMiddleWareRequestData): IPromiseMayBe<ICommonReturn | Partial<IMiddleWareRequestData>> {
+    request (req: IMiddleWareRequestData): IPromiseMayBe<ICommonReturn | Partial<IMiddleWareRequestData>> {
+        const { url, method, send404 } = req;
         const key = this.buildRouteKey(url, method);
         // console.log('on request', key);
-        if (!!this.routers[key]) {
+        const route = this.routers[key];
+        if (!!route) {
             return MiddleWareReturn.Continue;
         }
         send404(`Page not found: ${url}`);
@@ -77,8 +115,8 @@ export class Router extends MiddleWare {
     response (res: IMiddleWareResponseData): IPromiseMayBe<ICommonReturn | IMiddleWareResponseReturn<any>> {
         const key = this.buildRouteKey(res.url, res.method);
         // console.log('on response', key);
-        const handler = this.routers[key];
-        if (!handler) {
+        const route = this.routers[key];
+        if (!route) {
             res.send404(`Page not found: ${res.url}`);
             // res.sendHtml(`<h1>Page not found: ${res.url}<jh1>`);
             return MiddleWareReturn.Return;
@@ -86,18 +124,37 @@ export class Router extends MiddleWare {
 
         let index = 0; // 路由中加入一个自增index，可以用于生成错误码 id等
         res.index = () => index++;
+        res.meta = route.meta;
         res.route = (url, data) => {
-            const handler = this.routers[this.fillUrl(url)];
-            if (!handler) {
+            const route = this.routers[this.fillUrl(url)];
+            if (!route) {
                 res.send404(`Route Dismiss: ${url}`);
                 return MiddleWareReturn.Return;
             }
             if (data) {
-                return handler(Object.assign({}, res, data));
+                return route.exe(Object.assign({}, res, data));
             }
-            return handler(res);
+            return route.exe(res);
         };
 
-        return handler(res);
+        return route.exe(res);
+    }
+
+    private _injectHelper(obj: any){
+
+        let index = 0; // 路由中加入一个自增index，可以用于生成错误码 id等
+        obj.index = () => index++;
+        obj.meta = route.meta;
+        obj.route = (url, data) => {
+            const route = this.routers[this.fillUrl(url)];
+            if (!route) {
+                res.send404(`Route Dismiss: ${url}`);
+                return MiddleWareReturn.Return;
+            }
+            if (data) {
+                return route.exe(Object.assign({}, res, data));
+            }
+            return route.exe(res);
+        };
     }
 }
